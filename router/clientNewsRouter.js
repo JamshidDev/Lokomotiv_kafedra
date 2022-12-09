0f66e1cbb308566f308b3d159152ac1be25670e9
JamshidDev <raximovl662@gmail.com>
2022-12-09 12:03:22 +0500

    added recently news api

--- router/clientNewsRouter.js
+++ router/clientNewsRouter.js
@@ -5,19 +5,130 @@ const langList = require("../utils/langList")
 const { NewEN, NewRU, NewUZ } = require("../models/newsModels")
 
 // Lenta news
-router.get('/lenta', async (req, res) =>{
-   try{
+router.get('/lenta', async (req, res) => {
+    try {
 
-    let per_page = req.query.per_page || 10;
+        let per_page = req.query.per_page || 10;
+        let lang_code = req.lang_code;
+        let news = null;
+
+        if (lang_code == langList[0].code) {
+            news = await NewUZ.find({}).sort({ created_date: -1 }).limit(per_page)
+        } else if (lang_code == langList[1].code) {
+            news = await NewRU.find({}).sort({ created_date: -1 }).limit(per_page)
+        } else if (lang_code == langList[2].code) {
+            news = await NewEN.find({}).sort({ created_date: -1 }).limit(per_page)
+        }
+
+        res.status(200).json({
+            isSuccess: true,
+            data: news,
+            errorMessage: null,
+        })
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            isSuccess: false,
+            data: null,
+            errorMessage: error,
+        });
+    }
+})
+
+
+// All news
+router.get('/all', async (req, res) => {
+    try {
+        let page = req.query.page || 1;
+        let per_page = req.query.per_page || 10;
+        let sort = req.query.sort || 1;
+        let lang_code = req.lang_code;
+        let news = null;
+
+        if (lang_code == langList[0].code) {
+            news = await NewUZ.find({}).sort({ created_date: sort }).skip((page - 1) * per_page).limit(per_page)
+        } else if (lang_code == langList[1].code) {
+            news = await NewRU.find({}).sort({ created_date: sort }).skip((page - 1) * per_page).limit(per_page)
+        } else if (lang_code == langList[2].code) {
+            news = await NewEN.find({}).sort({ created_date: sort }).skip((page - 1) * per_page).limit(per_page)
+        }
+
+        res.status(200).json({
+            isSuccess: true,
+            data: news,
+            errorMessage: null,
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            isSuccess: false,
+            data: null,
+            errorMessage: error,
+        });
+    }
+})
+
+// Get one news
+router.get('/one', async (req, res) => {
+    try {
+
+        let news_id = req.query.news_id;
+        let lang_code = req.lang_code;
+        let news = null;
+        let oldNews = null;
+
+        if (lang_code == langList[0].code) {
+            oldNews = await NewUZ.findById(news_id)
+            news = await NewUZ.findByIdAndUpdate(news_id, {
+                views: oldNews.views + 1,
+            })
+        } else if (lang_code == langList[1].code) {
+            oldNews = await NewRU.findById(news_id)
+            news = await NewRU.findByIdAndUpdate(news_id, {
+                views: oldNews.views + 1,
+            })
+        } else if (lang_code == langList[2].code) {
+            oldNews = await NewEN.findById(news_id)
+            news = await NewEN.findByIdAndUpdate(news_id, {
+                views: oldNews.views + 1,
+            })
+        }
+
+        res.status(200).json({
+            isSuccess: true,
+            data: news,
+            errorMessage: null,
+        })
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            isSuccess: false,
+            data: null,
+            errorMessage: error,
+        });
+    }
+})
+
+router.get("/recnetly", async (req, res) => {
+   try{
     let lang_code = req.lang_code;
+    let sort = req.query.sort || 1;
+    let per_page = req.query.per_page || 10; 
     let news = null;
 
-    if(lang_code == langList[0].code){
-        news = await NewUZ.find({}).sort({created_date:-1}).limit(per_page)
-    }else if(lang_code == langList[1].code){
-        news = await NewRU.find({}).sort({created_date:-1}).limit(per_page)
-    }else if(lang_code == langList[2].code){
-        news = await NewEN.find({}).sort({created_date:-1}).limit(per_page)
+
+
+    if (lang_code == langList[0].code) {
+        news = await NewUZ.find({}).sort(sort).limit(per_page);
+    } else if (lang_code == langList[1].code) {
+        news = await NewUZ.find({}).sort(sort).limit(per_page);
+    } else if (lang_code == langList[2].code) {
+        news = await NewUZ.find({}).sort(sort).limit(per_page);
     }
 
     res.status(200).json({
@@ -27,86 +138,18 @@ router.get('/lenta', async (req, res) =>{
     })
 
 
+
    }catch(error){
+
     console.log(error);
     res.status(500).json({
-      isSuccess: false,
-      data: null,
-      errorMessage: error,
+        isSuccess: false,
+        data: null,
+        errorMessage: error,
     });
+
    }
 })
-
-
-// All news
-router.get('/all', async (req, res) =>{
-    try{
-        let page = req.query.page || 1;
-        let per_page = req.query.per_page || 10;
-        let sort = req.query.sort || 1;
-        let lang_code = req.lang_code;
-        let news =null;
-
-        if(lang_code == langList[0].code){
-            news = await NewUZ.find({}).sort({created_date:sort}).skip((page-1)*per_page).limit(per_page)
-        }else if(lang_code == langList[1].code){
-            news = await NewRU.find({}).sort({created_date:sort}).skip((page-1)*per_page).limit(per_page)
-        }else if(lang_code == langList[2].code){
-            news = await  NewEN.find({}).sort({created_date:sort}).skip((page-1)*per_page).limit(per_page)
-        }
-    
-        res.status(200).json({
-            isSuccess: true,
-            data: news,
-            errorMessage: null,
-        })
-                        
-    }catch(error){
-        console.log(error);
-        res.status(500).json({
-          isSuccess: false,
-          data: null,
-          errorMessage: error,
-        });
-    }
-})
-
-// Get one news
-router.get('/one', async (req, res) =>{
-    try{
- 
-     let news_id = req.query.news_id;
-     let lang_code = req.lang_code;
-     let news = null;
- 
-     if(lang_code == langList[0].code){
-         news = await NewUZ.findById(news_id)
-     }else if(lang_code == langList[1].code){
-         news = await NewRU.findById(news_id)
-     }else if(lang_code == langList[2].code){
-         news = await NewEN.findById(news_id)
-     }
- 
-     res.status(200).json({
-         isSuccess: true,
-         data: news,
-         errorMessage: null,
-     })
- 
- 
-    }catch(error){
-     console.log(error);
-     res.status(500).json({
-       isSuccess: false,
-       data: null,
-       errorMessage: error,
-     });
-    }
- })
-
- router.get("/recnetly", async (req,res) =>{
-    res.send(req.url)
- })
 
 
 
